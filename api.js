862aefac94f82db4aed0194a9459b19cbbf2588c
Vladyslav <baskov228322@gmail.com>
2022-04-22 11:06:45 +0300

    1106

--- api.js
+++ api.js
@@ -2,7 +2,16 @@ APIKEY = '244ce0346be4b684cc59285d0ab99682';
 let obj;
 fetch('https://api.openweathermap.org/data/2.5/weather?id=703448&appid=' + APIKEY)
     .then(response => response.json())
-    .then((data) => { obj = data })
-    .then(() => console.log(obj))
+    .then((data) => {
+
+        
+    })
     .catch(err => console.log(err));
+
+
+function getLocation() {
+    var geolocation = navigator.geolocation;
+    geolocation.getCurrentPosition((position) => {console.log(position)});
+}
+
 
