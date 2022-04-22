e5ff79999b2a12e7f2cc093c790e4404f96aa3f4
Vladyslav <baskov228322@gmail.com>
2022-04-22 12:03:43 +0300

    1203

--- api.js
+++ api.js
@@ -1,17 +1,25 @@
-APIKEY = '244ce0346be4b684cc59285d0ab99682';
+let APIKEY = '244ce0346be4b684cc59285d0ab99682';
 let obj;
-fetch('https://api.openweathermap.org/data/2.5/weather?id=703448&appid=' + APIKEY)
+
+
+const getLoc = new Promise((resolve, reject) => {
+    let geolocation = navigator.geolocation;
+
+    geolocation.getCurrentPosition((position) => { resolve(position) });
+
+})
+
+
+getLoc.then(data => {
+    const lat = (data.coords.latitude.toFixed(2));
+    const lon = (data.coords.longitude.toFixed(2));
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`)
     .then(response => response.json())
     .then((data) => {
-
-        
+        console.log(data);
     })
     .catch(err => console.log(err));
 
-
-function getLocation() {
-    var geolocation = navigator.geolocation;
-    geolocation.getCurrentPosition((position) => {console.log(position)});
-}
-
+})
 
