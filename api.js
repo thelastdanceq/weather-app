e3dba214a03124260df975f8d38dcfa86d38b37a
Vladyslav <baskov228322@gmail.com>
2022-04-23 15:12:18 +0300

    1512

--- api.js
+++ api.js
@@ -14,21 +14,63 @@ getLoc.then(data => {
     const lat = (data.coords.latitude.toFixed(2));
     const lon = (data.coords.longitude.toFixed(2));
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`)
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data);
-            let nameField = document.querySelector('#name');
-            let degreeField = document.querySelector('#degree');
-            let statusField = document.querySelector('#status');
-
-            nameField.innerHTML = data.name;
-            degreeField.innerHTML = Math.round(data.main.temp - 273) + ' &deg;';
-            statusField.innerHTML = data.weather[0]['description']
-
-            
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${APIKEY}`)
+        .then(response => {
+            return response.json()
         })
-        .catch(err => console.log(err));
+        .then(data => {
+            console.log(data);
+            let forecast = document.querySelector('#forecast');
+            data.daily.forEach(date => {
+                let div = document.createElement('div');
+                div.classList.add('day');
+
+                let dateDt = new Date(date.dt * 1000);
+                let day = dateDt.getDay();
+                switch (day) {
+                    case 6:
+                        div.innerHTML = 'Sunday';
+                        break;
+
+                    case 5:
+                        div.innerHTML = 'Saturday';
+
+                        break;
+                    case 4:
+
+                        div.innerHTML = 'Friday';
+
+                        break;
+                    case 3:
+                        div.innerHTML = 'Thursday';
+
+                        break;
+                    case 2:
+                        div.innerHTML = 'Wednesday';
+
+                        break;
+                    case 1:
+                        div.innerHTML = 'Tuesday';
+
+                        break;
+                    case 0:
+                        div.innerHTML = 'Monday';
+
+                        break;
+                }
+                let temp = Math.round(date.temp.day - 273);
+                let p = document.createElement('p');
+                p.innerHTML = temp + '&deg;';
+                div.appendChild(p)
+                forecast.appendChild(div);
+            });
+        })
+        .catch(err => {
+            console.error(err);
+        });
 
 })
+
+
+
 
