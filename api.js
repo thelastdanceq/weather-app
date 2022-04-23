2cd1ed26581db2cbd61dc40ea5a68eca71522579
Vladyslav <baskov228322@gmail.com>
2022-04-23 19:55:10 +0300

    1955

--- api.js
+++ api.js
@@ -14,57 +14,63 @@ getLoc.then(data => {
     const lat = (data.coords.latitude.toFixed(2));
     const lon = (data.coords.longitude.toFixed(2));
 
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${APIKEY}`)
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,current,hourly,alerts&appid=${APIKEY}`)
         .then(response => {
             return response.json()
         })
         .then((data) => {
             console.log(data);
-            let forecast = document.querySelector('#forecast');
-            data.daily.forEach((date, index) => {
 
+            let wrapper = document.querySelector('wrapper');
+            let forecast = document.querySelector('#forecast');
+
+            let place = document.createElement('p');
+            data.daily.forEach((date, index) => {
                 let div = document.createElement('div');
                 div.classList.add('day');
-                if (index === 0) div.classList.add('now');
-
-                let dateDt = new Date(date.dt * 1000);
-                let day = dateDt.getDay();
-                switch (day) {
-                    case 6:
-                        div.innerHTML = 'Sunday';
-                        break;
-
-                    case 5:
-                        div.innerHTML = 'Saturday';
-
-                        break;
-                    case 4:
-
-                        div.innerHTML = 'Friday';
-
-                        break;
-                    case 3:
-                        div.innerHTML = 'Thursday';
-
-                        break;
-                    case 2:
-                        div.innerHTML = 'Wednesday';
-
-                        break;
-                    case 1:
-                        div.innerHTML = 'Tuesday';
-
-                        break;
-                    case 0:
-                        div.innerHTML = 'Monday';
-
-                        break;
+                if(index == 0){
+                    return;
                 }
-                let temp = Math.round(date.temp.day - 273);
-                let p = document.createElement('p');
-                p.innerHTML = temp + '&deg;';
-                div.appendChild(p)
-                forecast.appendChild(div);
+
+                    let dateDt = new Date(date.dt * 1000);
+                    let day = dateDt.getDay();
+                    switch (day) {
+                        case 6:
+                            div.innerHTML = 'Sunday';
+                            break;
+
+                        case 5:
+                            div.innerHTML = 'Saturday';
+
+                            break;
+                        case 4:
+
+                            div.innerHTML = 'Friday';
+
+                            break;
+                        case 3:
+                            div.innerHTML = 'Thursday';
+
+                            break;
+                        case 2:
+                            div.innerHTML = 'Wednesday';
+
+                            break;
+                        case 1:
+                            div.innerHTML = 'Tuesday';
+
+                            break;
+                        case 0:
+                            div.innerHTML = 'Monday';
+
+                            break;
+                    }
+                    let temp = Math.round(date.temp.day - 273);
+                    let p = document.createElement('p');
+                    p.innerHTML = temp + '&deg;';
+                    div.appendChild(p)
+                    forecast.appendChild(div);
+                
             });
         })
         .catch(err => {
