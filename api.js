fcb4e68845467aca6c3a376b38c9deb66c8c43b8
Vladyslav <baskov228322@gmail.com>
2022-04-24 12:27:08 +0300

    1227

--- api.js
+++ api.js
@@ -21,24 +21,47 @@ getLoc.then(data => {
                 .then((response) => { return response.json() })
                 .then((data1) => {
                     console.log(data1);
-                    let wrapper = document.querySelector('#wrapper');
-                    let forecast = document.querySelector('#forecast');
-                    let div = document.createElement('div');
-                    div.classList.add('day', 'now');
-
-                    let place = document.createElement('span');
-                    place.innerHTML = data1.name;
+                    const now = document.querySelector('#now');
                     let tempNow = document.createElement('p');
 
-                    tempNow.innerHTML = Math.round(data1.main.temp - 273) + ' &deg;';
-                    div.appendChild(place);
-                    div.appendChild(document.createElement('br'));
-                    div.appendChild(tempNow);
+                    tempNow.innerHTML = Math.round(data1.main.temp - 273) + '&deg;';
+                    let image;
+                    switch (data1.weather[0].main) {
+                        case 'Clouds':
+                            if (data1.main.temp - 273 > 0 && data1.main.temp - 273 <= 20) {
+                                image = "/images/photo_2022-04-24_12-24-43-removebg-preview.png"
+                            }
+                            break;
 
+                        case 'Clear':
+                            image = "/images/icons8-солнце-16.png"
+                            break;
+                        case 'Snow':
+                            image = "/images/icons8-метель-16.png"
+                            break;
+                        case "Rain":
+                            if (data1.main.temp - 273 > 0 && data1.main.temp - 273 <= 20) {
+                                image = "/images/photo_2022-04-24_11-10-06-removebg-preview.png"
+                            }
+                            break;
+                        case "Drizzle":
+                            image = "/images/icons8-небольшая-облачность-дождь-16.png"
+                            break;
+                        case "Thunderstorm":
+                            image = "/images/icons8-молния-из-облаков-16.png"
+                            break;
+                    }
+                    let imageNowDiv = document.createElement('div');
+                    let imageNow = document.createElement('img');
+                    imageNow.src = image;
+                    let place = document.createElement('p');
+                    place.innerHTML = data1.name
+                    imageNow.height = '300';
+                    imageNowDiv.appendChild(imageNow);
+                    now.appendChild(place);
+                    now.appendChild(imageNowDiv)
+                    now.appendChild(tempNow)
 
-
-                    wrapper.appendChild(div);
-                    wrapper.insertBefore(div, forecast);
 
                 });
 
@@ -50,46 +73,79 @@ getLoc.then(data => {
             data.daily.forEach((date, index) => {
                 let div = document.createElement('div');
                 div.classList.add('day');
-                if (index == 0) {
-                    return;
-                }
-
+                let imageDay
                 let dateDt = new Date(date.dt * 1000);
                 let day = dateDt.getDay();
                 switch (day) {
                     case 6:
-                        div.innerHTML = 'Sunday';
+                        div.innerHTML = 'Воскресение';
                         break;
 
                     case 5:
-                        div.innerHTML = 'Saturday';
+                        div.innerHTML = 'Суббота';
 
                         break;
                     case 4:
 
-                        div.innerHTML = 'Friday';
+                        div.innerHTML = 'Пятница';
 
                         break;
                     case 3:
-                        div.innerHTML = 'Thursday';
+                        div.innerHTML = 'Четверг';
 
                         break;
                     case 2:
-                        div.innerHTML = 'Wednesday';
+                        div.innerHTML = 'Среда';
 
                         break;
                     case 1:
-                        div.innerHTML = 'Tuesday';
+                        div.innerHTML = 'Вторник';
 
                         break;
                     case 0:
-                        div.innerHTML = 'Monday';
+                        div.innerHTML = 'Понедельник';
 
                         break;
                 }
                 let temp = Math.round(date.temp.day - 273);
+                switch (date.weather[0].main) {
+                    case 'Clouds':
+                        console.log(date.temp.day - 273);
+                        if (date.temp.day - 273 > 0 && date.temp.day - 273 <= 20) {
+                            imageDay = "/images/photo_2022-04-24_12-24-43-removebg-preview.png"
+                        }
+                        break;
+
+                    case 'Clear':
+                        if (date.temp.day - 273 > 0 && date.temp.day - 273 <= 20) {
+                            imageDay = "/images/photo_2022-04-24_12-19-16-removebg-preview.png"
+                        }
+                        break;
+                    case 'Snow':
+                        imageDay = "/images/icons8-метель-16.png"
+                        break;
+                    case "Rain":
+                        if (temp > 0 && temp <= 20) {
+                            imageDay = "/images/photo_2022-04-24_11-10-06-removebg-preview.png"
+                        }
+                        break;
+                    case "Drizzle":
+                        imageDay = "/images/icons8-небольшая-облачность-дождь-16.png"
+                        break;
+                    case "Thunderstorm":
+                        imageDay = "/images/icons8-молния-из-облаков-16.png"
+                        break;
+                }
+
                 let p = document.createElement('p');
                 p.innerHTML = temp + '&deg;';
+                let imgDay = document.createElement('img');
+                let imgDayDiv = document.createElement('div');
+                imgDayDiv.appendChild(imgDay);
+                imgDay.src = imageDay;
+                imgDay.classList.add('image');
+                imgDay.height = '250';
+                div.appendChild(imgDayDiv);
                 div.appendChild(p)
                 forecast.appendChild(div);
 
@@ -103,6 +159,4 @@ getLoc.then(data => {
 
 
 
-function chooseImage(temp, weather, windSpeed) {
 
-}
